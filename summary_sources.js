26713090cc5d0bab4a20544a4f6fd0f5d5d72474
Arnav <78939786+arnard76@users.noreply.github.com>
2022-07-16 13:55:51 +1200

    Idea for populating content from news sites
    Quick idea using the dom selector queries

---
+++ summary_sources.js
@@ -0,0 +1,5 @@
+sources = [
+    {url: 'https://www.stuff.co.nz/', findingContent: [{contentRole: 'summary', domSelectorQuery: 'app-headline'}]},
+    {url: 'https://web.dev/blog/', findingContent: [{contentRole: 'summary', domSelectorQuery: '.card__heading'}]},
+
+]
